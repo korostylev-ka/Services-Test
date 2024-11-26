060c18c5b42e7eeda67fa207aabadb1137370813
k.korostylev <k.korostylev@gmail.com>
2024-11-26 10:58:35 +0500

    Job Service enqueue

--- app/src/main/java/ru/sumin/servicestest/MainActivity.kt
+++ app/src/main/java/ru/sumin/servicestest/MainActivity.kt
@@ -5,6 +5,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.job.JobInfo
 import android.app.job.JobScheduler
+import android.app.job.JobWorkItem
 import android.content.ComponentName
 import android.os.Build
 import android.os.Bundle
@@ -19,6 +20,8 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private var page = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,12 +44,19 @@ class MainActivity : AppCompatActivity() {
             val jobInfo = JobInfo.Builder(MyJobService.JOB_ID, componentName)
 //                .setRequiresCharging(true)
 //                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED) //только от wi-fi
-                .setPersisted(true) //автоматический запуск сервиса после перезагрузки устройства
+//                .setExtras(MyJobService.newBundle(page++))
+                //.setPersisted(true) //автоматический запуск сервиса после перезагрузки устройства
                 .build()
 
             val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
 
-            jobScheduler.schedule(jobInfo)
+            //каждый последующий сервис отменяет работу предыдущего
+//            jobScheduler.schedule(jobInfo)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val intent = MyJobService.newIntent(page++)
+                jobScheduler.enqueue(jobInfo, JobWorkItem(intent))
+            }
         }
     }
 
--- app/src/main/java/ru/sumin/servicestest/MyJobService.kt
+++ app/src/main/java/ru/sumin/servicestest/MyJobService.kt
@@ -5,7 +5,9 @@ import android.app.job.JobParameters
 import android.app.job.JobService
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
+import android.os.PersistableBundle
 import android.util.Log
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -17,15 +19,41 @@ class MyJobService: JobService() {
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
     override fun onStartJob(p0: JobParameters?): Boolean {
-        coroutineScope.launch {
-            for (i in 0 until 100) {
-                delay(1000)
-                log("Timer $i")
-            }
-//            true если нужно будет перезапустить сервис через какое то время
-            jobFinished(p0, true)
-        }
         log("onStartJob")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            coroutineScope.launch {
+                var workItem = p0?.dequeueWork()
+                while (workItem != null) {
+
+                    val page = workItem.intent.getIntExtra(PAGE, 0)
+                    log("$page")
+                    for (i in 0 until 5) {
+                        delay(1000)
+                        log("Timer $i $page")
+                    }
+                    //завершаем текущую очередь
+                    p0?.completeWork(workItem)
+                    workItem = p0?.dequeueWork()
+
+                }
+                jobFinished(p0, false)
+            }
+
+
+        }
+//        через PersistableBundle
+//        val page = p0?.extras?.getInt(PAGE) ?: 0
+//
+//        coroutineScope.launch {
+//            for (i in 0 until 5) {
+//                delay(1000)
+//                log("Timer $i $page")
+//            }
+//
+////            true если нужно будет перезапустить сервис через какое то время
+//            jobFinished(p0, true)
+//        }
+//        log("onStartJob")
 //        возвращаем true если сервис еще выполняется(например асинхронная работа)
         return true
     }
@@ -56,6 +84,20 @@ class MyJobService: JobService() {
     companion object {
 
         const val JOB_ID = 11
+        private const val PAGE = "page"
+
+//        для запоминания состояния
+//        fun newBundle(page: Int): PersistableBundle {
+//            return PersistableBundle().apply {
+//                putInt(PAGE, page)
+//            }
+//        }
+        fun newIntent(page: Int): Intent {
+            return Intent().apply {
+                putExtra(PAGE, page)
+        }
+}
+
     }
 
 
